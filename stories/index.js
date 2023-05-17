652aa2495a40454883e24c7ec242ad60534e6150
Tony Chow <tchow9@gmail.com>
2023-05-16 17:51:12 -0700

    Add create and edit form in appointments

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -163,4 +164,20 @@ storiesOf("Appointment", module)
       message={"Could not delete appointment."}
       onClose={action("onClose")}
     />
-  ));
+  ))
+  .add("Edit", () => (
+    <Form
+      student={"Tony Chow"}
+      interviewer={1}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Create", () => {
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />;
+  });
