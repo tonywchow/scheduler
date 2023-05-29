38d56ec7c8e438bbb755fd0c8f7ffca218ffe08b
Tony Chow <tchow9@gmail.com>
2023-05-29 15:31:01 -0700

    Refactor useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  // const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   function transition(mode, replace = false) {
@@ -9,13 +8,11 @@ export default function useVisualMode(initial) {
       return setHistory((prev) => [...prev.slice(0, -1), mode]);
     }
     setHistory((prev) => [...prev, mode]);
-    // setMode(mode);
   }
 
   function back() {
     if (history.length > 1) {
       setHistory((prev) => [...prev.slice(0, -1)]);
-      // setMode(history[history.length - 1]);
     }
   }
   return { mode: history[history.length - 1], transition, back };
