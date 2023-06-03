edd34d8fa37e3507bbdb753d03fe793492091523
Tony Chow <tchow9@gmail.com>
2023-06-02 20:32:18 -0700

    Add updateSpot feature to save function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -9,6 +9,50 @@ const useApplicationData = () => {
     interviewers: {},
   });
 
+
+  //user creates appointment
+  //using axios sends data to api URL
+  //on success: response from api server
+  //calculate number of spots from response (function)
+  //on error: console.log(error)/transition(ERROR_SAVE)
+
+  //how to calculate the number of spots:
+  //parameters Obj
+  //let space = 0;
+  //state.day === day.name
+  //dayArr = day.appointments  //Array
+  //for(let appointmentID of dayArr) {
+  //if(appointments[appointmentID].interview === null) {
+  //space += 1
+  //return space;
+  //let appointment = { ...appointment, appointment.interview}
+
+  function updateSpot(obj) {
+    let space = 0;
+    let dayArr = [];
+    //grabbing the array of appointment IDs if it equal to day selected
+    for (let day of state.days) {
+      if (day.name === state.day) {
+        dayArr = day.appointments;
+      }
+    }
+    for (let appointmentID of dayArr) {
+      if (obj[appointmentID].interview === null) {
+        space += 1;
+      }
+    }
+    return space;
+  }
+
+  let dayObjectID = {
+    Monday: 0,
+    Tuesday: 1,
+    Wednesday: 2,
+    Thursday: 3,
+    Friday: 4,
+  };
+
+
   const apiURLS = {
     GET_DAYS: "http://localhost:8001/api/days",
     GET_APPOINTMENTS: "http://localhost:8001/api/appointments",
@@ -21,16 +65,22 @@ const useApplicationData = () => {
       ...state.appointments[id],
       interview: { ...interview },
     };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
+    // const appointments = {
+    //   ...state.appointments,
+    //   [id]: appointment,
+    // };
 
     return axios
       .put(`http://localhost:8001/api/appointments/${id}`, appointment)
       .then((res) => {
-        setState((prev) => ({ ...prev, appointments }));
-        console.log("saving", res);
+        setState((prev) => {
+          let newState = { ...prev };
+          newState.appointments[id] = appointment;
+          newState.days[dayObjectID[state.day]].spots = updateSpot(
+            newState.appointments
+          );
+          return newState;
+        });
       });
   }
 
@@ -48,6 +98,8 @@ const useApplicationData = () => {
     return axios
       .delete(`http://localhost:8001/api/appointments/${id}`)
       .then((res) => {
+        // setSpots(spots + 1);
+        // let spotRefresh = updateSpots(1);
         setState((prev) => ({ ...prev, appointments }));
       });
   }
@@ -66,6 +118,7 @@ const useApplicationData = () => {
       }));
     });
   }, [apiURLS.GET_DAYS, apiURLS.GET_APPOINTMENTS, apiURLS.GET_INTERVIEWERS]);
+
   return { bookInterview, cancelInterview, state, setDay };
 };
 
