44682a56d3d5d45dd6d9d07c1b07a0ecbab00cf6
Tony Chow <tchow9@gmail.com>
2023-06-10 16:04:22 -0700

    Add navigation cypress test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,21 @@
+// beforeEach(() => {
+//   cy.visit("/");
+// });
+
 describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+// Find the list item that has a descendent with the text content "Tuesday" and click
+    cy.get("li").contains("Tuesday").click();
+//Allows us to check that "Tuesday" list item has a particular off white background colour
+    cy.contains("li", "Tuesday").should(
+      "have.css",
+      "background-color",
+      "rgb(242, 242, 242)"
+    );
   });
 });
