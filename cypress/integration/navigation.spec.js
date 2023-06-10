db9631f0bc63f08eb0ead6162b485e7800662a95
Tony Chow <tchow9@gmail.com>
2023-06-10 16:55:33 -0700

    Refactor navigation test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -9,13 +9,10 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-// Find the list item that has a descendent with the text content "Tuesday" and click
-    cy.get("li").contains("Tuesday").click();
-//Allows us to check that "Tuesday" list item has a particular off white background colour
-    cy.contains("li", "Tuesday").should(
-      "have.css",
-      "background-color",
-      "rgb(242, 242, 242)"
-    );
+    // Find the list item that has a descendent with the text content "Tuesday" and click
+    //Allows us to check that "Tuesday" list item has a particular off white background colour
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
