cb9c0c3a692f730c80d0a632f42253d3930f4a0d
Tony Chow <tchow9@gmail.com>
2023-06-11 11:17:07 -0700

    Delete commented code

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,7 +1,3 @@
-// beforeEach(() => {
-//   cy.visit("/");
-// });
-
 describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
@@ -10,9 +6,9 @@ describe("Navigation", () => {
   it("should navigate to Tuesday", () => {
     cy.visit("/");
     // Find the list item that has a descendent with the text content "Tuesday" and click
-    //Allows us to check that "Tuesday" list item has a particular off white background colour
     cy.contains("[data-testid=day]", "Tuesday")
       .click()
+      //Allows us to check that "Tuesday" list item has a particular off white background colour
       .should("have.class", "day-list__item--selected");
   });
 });
